67b9e4c79766f5a9463095bd14b4e491ac70993c
nirajprakash <nirprjj@gmail.com>
2017-06-23 20:00:48 +0530

    updated projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,17 +17,17 @@ import {Ng2PageScrollModule} from 'ng2-page-scroll';
 import { SampleModule } from './ui/sample/sample.module';
 import {ServicePortfolioApi, ServiceWindow, ServiceClipboard} from './services';
 const routes:  Routes = [
-  { path: '', redirectTo: '/test2', pathMatch: 'full'},
+  { path: '', redirectTo: '/onepage', pathMatch: 'full'},
   { path: 'onepage', loadChildren: './ui/onepage/onepage.module#OnepageModule' },
-  { path: 'footer', loadChildren: './ui/footer/footer.module#FooterModule' },
+/*  { path: 'footer', loadChildren: './ui/footer/footer.module#FooterModule' },
   { path: 'sample', loadChildren: './ui/sample/sample.module#SampleModule' },
   { path: 'profile', loadChildren: './ui/profile/profile.module#ProfileModule' },
   { path: 'home', loadChildren: './ui/home/home.module#HomeModule' },
   { path: 'portfolio', loadChildren: './ui/portfolio/portfolio.module#PortfolioModule' },
   { path: 'about', loadChildren: './ui/about/about.module#AboutModule' },
-  { path: 'contact', loadChildren: './ui/contact/contact.module#ContactModule' },
-  { path: 'test', loadChildren: './ui/test/test.module#TestModule' },
-  { path: 'test2', loadChildren: './ui/test2/test2.module#Test2Module' }
+  { path: 'contact', loadChildren: './ui/contact/contact.module#ContactModule' },*/
+//  { path: 'test', loadChildren: './ui/test/test.module#TestModule' },
+//  { path: 'test2', loadChildren: './ui/test2/test2.module#Test2Module' }
 //{ path: 'sample', loadChildren: ()=> SampleModule }
 ];
 const rootRouting: ModuleWithProviders = RouterModule.forRoot(routes, { useHash: true, enableTracing: true });
